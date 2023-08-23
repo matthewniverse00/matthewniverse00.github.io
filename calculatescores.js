73a13144d8abce08b4fb29c2a59479dae05fa121
Matthew Polland <matthew.polland.2020@uni.strath.ac.uk>
2023-08-23 12:11:07 +0100

    bug fixes

--- calculatescores.js
+++ calculatescores.js
@@ -1,21 +1,22 @@
 function calculate() {
 
-    let Guesses = parseFloat(document.getElementById('inputC1').value);
-    let Greens = parseFloat(document.getElementById('inputC2').value);
-    let Yellows = parseFloat(document.getElementById('inputC3').value);
+    let guesses = parseFloat(document.getElementById('inputC1').value);
+    let greens = parseFloat(document.getElementById('inputC2').value);
+    let yellows = parseFloat(document.getElementById('inputC3').value);
 
-    let Wordle = Guesses * (1/2*Greens + 1/Yellows);
-    let One = parseFloat(document.getElementById('inputCOne').value);
+    let wordle = guesses * (1/2*greens + 1/yellows);
+    let one = parseFloat(document.getElementById('inputCOne').value);
 
-    let scoreC = Math.round((Math.sqrt((Wordle)*(Wordle) + (One)*(One)) + Number.EPSILON) * 100) / 100;
+    let scoreC = Math.round((Math.sqrt((wordle)*(wordle) + (one/2)*(one/2)) + Number.EPSILON) * 100) / 100;
 
-    Guesses = parseFloat(document.getElementById('inputM1').value);
-    Greens = parseFloat(document.getElementById('inputM2').value);
-    Yellows = parseFloat(document.getElementById('inputM3').value);
+    guesses = parseFloat(document.getElementById('inputM1').value);
+    greens = parseFloat(document.getElementById('inputM2').value);
+    yellows = parseFloat(document.getElementById('inputM3').value);
 
-    Wordle = Guesses * (1/2*Greens + 1/Yellows);
+    wordle = guesses * (1/2*greens + 1/yellows)
+    one = parseFloat(document.getElementById('inputMOne').value);
 
-    let scoreM = Math.round((Math.sqrt((Wordle)*(Wordle) + (One)*(One)) + Number.EPSILON) * 100) / 100;
+    let scoreM = Math.round((Math.sqrt((wordle)*(wordle) + (one/2)*(one/2)) + Number.EPSILON) * 100) / 100;
 
     document.getElementById('scoresC').textContent = 'Ciara score: ' + scoreC;
     document.getElementById('scoresM').textContent = 'Matthew score: ' + scoreM;
